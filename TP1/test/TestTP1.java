887936d06483eb16f661a6c93979166b5727aa4f
William Garrier <william.garrier63@orange.fr>
2020-12-08 15:41:43 +0100

    Project setup

---
+++ TP1/test/TestTP1.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class TestTP1 {
+    @Test
+    public void working() {
+        Assertions.assertTrue(true);
+    }
+}
